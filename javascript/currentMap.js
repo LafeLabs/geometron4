6087ad358234bc944ee32f3899c67235c7792b5a
Lafe Spietz <lafelabs@gmail.com>
2019-04-29 23:38:23 -0400

    now both map editor and symbol edtitor have persistence, using js files with json var values

---
+++ javascript/currentMap.js
@@ -0,0 +1,49 @@
+currentMap = {
+    "name": "other main map",
+    "array": [
+        {
+            "x": 0.07875894988066826,
+            "y": 0.060859188544152745,
+            "w": 0.8770883054892601,
+            "h": 1,
+            "angle": 0,
+            "text": "",
+            "href": "",
+            "url": "http://geometron.000webhostapp.com/uploadimages/image27.jpg",
+            "type": "image"
+        },
+        {
+            "x": 0.36873508353221957,
+            "y": 0.18854415274463007,
+            "w": 0.14916467780429593,
+            "h": 1,
+            "angle": 50,
+            "text": "TOP top top",
+            "href": "",
+            "url": "",
+            "type": "text"
+        },
+        {
+            "x": 0.1610978520286396,
+            "y": 0.6264916467780429,
+            "w": 0.14916467780429593,
+            "h": 1,
+            "angle": -52,
+            "text": "BOTTOM",
+            "href": "",
+            "url": "",
+            "type": "text"
+        },
+        {
+            "x": 0.6312649164677804,
+            "y": 0.2756563245823389,
+            "w": 0.09904534606205251,
+            "h": 1,
+            "angle": 180,
+            "text": "",
+            "href": "index.html",
+            "url": "http://www.cherrytreemaryland.xyz/symbol/svg/svg1556330629.svg",
+            "type": "image"
+        }
+    ]
+};
